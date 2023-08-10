b87cce1ca2012795d46dbe55c14fe1717e2fbe3b
pikmin61 <pikmin61@gmail.com>
2023-08-10 18:03:24 -0400

    lab3 and 4
    should've uploaded these awhile back

---
+++ Powershell/Lab3.ps1
@@ -0,0 +1,3 @@
+﻿$networkadapters = get-ciminstance win32_networkadapterconfiguration | Where-Object ipenabled
+
+$networkadapters | Select-Object Description,index,ipaddress,ipsubnet,dnsdomain,DNSServerSearchOrder |  format-table -AutoSize -Wrap
